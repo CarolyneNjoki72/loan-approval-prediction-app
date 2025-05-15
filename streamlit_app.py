d15501cbdfced4bab70d235217fc615f446027b3
CarolyneNjoki72 <161461568+CarolyneNjoki72@users.noreply.github.com>
2025-05-15 21:50:56 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,6 +36,6 @@ with st.sidebar:
   CoappliantIncome = st.slider('How much does your coappliant earn?', 0, 41667, 1621.25)
   LoanAmount = st.slider('How much are you applying?', 9, 700, 146)
   Loan_Amount_Term = st.slider('How long do you want to repay the loan?' 12, 480, 342)
-  Credit_History = st.selectbox('Do you have some credit history?, '1', '0')
+  Credit_History = st.selectbox('Do you have some credit history?, ('1', '0'))
   
   
