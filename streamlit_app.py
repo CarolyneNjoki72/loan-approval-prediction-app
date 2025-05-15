c0561a7e8ba2df595b9d75c95a9d5b2468c499b0
CarolyneNjoki72 <161461568+CarolyneNjoki72@users.noreply.github.com>
2025-05-15 21:22:45 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,19 +6,14 @@ st.title('🎈 Loan Approval Prediction App')
 st.info('This is an app built to predict loan approvals.')
 
 with st.expander('Data'):
-  st.write('**Raw data**')
-  df = pd.read_csv('https://raw.githubusercontent.com/CarolyneNjoki72/Loan-Prediction-Model/refs/heads/main/loan_prediction_cleancopy.csv')
-  df
-
-  st.write('**X**')
-  X = df.drop('Loan_Status', axis=1)
-  X
+#Data preparation
+with st.sidebar:
+  st.header('Input features')
+  #Gender,Married,Dependents,Education,Self_Employed,ApplicantIncome,CoapplicantIncome,LoanAmount,Loan_Amount_Term,Credit_History,Property_Area,Loan_Status
+  Gender = st.selectbox('What is your gender?',('Female', 'Male'))
+  Married = st.selectbox('Are you married?', ('Yes', 'No'))
+  Dependents = st.selectbox('How many dependents do you have?', ('1', '2', '3+'))
+  Education = st.selectbox('What is your highest education level?',('Graduate', 'Not Graduate')) 
+  Self_Employed = st.selectbox('Are you self employed?', ('Yes', 'No'))
+  ApplicantIncome = st.slider('How much is your income?', 150, 81000, 5403)
   
-  st.dataframe(df, hide_index=True)
-
-  st.write('**y**')
-  y = df.Loan_Status
-  y
-
-with st.expander('Data visualization'):
-  st.scatter_chart(data=df, x='ApplicantIncome', y='LoanAmount', color='Loan_Status')
