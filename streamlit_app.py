9a928952a62a026c7e89b6475f0ec259f05cb352
CarolyneNjoki72 <161461568+CarolyneNjoki72@users.noreply.github.com>
2025-05-15 17:15:07 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ st.title('🎈 Loan Approval Prediction App')
 
 st.info('This is an app built to predict loan approvals.')
 
-with st.expander('Data')
+with st.expander('Data'):
   st.write('**Raw data**')
   df = pd.read_csv('https://raw.githubusercontent.com/CarolyneNjoki72/Loan-Prediction-Model/refs/heads/main/loan_prediction_cleancopy.csv')
   df
