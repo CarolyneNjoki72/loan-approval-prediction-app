0d943640491bc5ea684e4096e2b61cb8f19cb2ee
CarolyneNjoki72 <161461568+CarolyneNjoki72@users.noreply.github.com>
2025-05-17 19:19:01 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,10 +22,40 @@ with st.expander('Data'):
 
 with st.expander('Data Visualization'):
   st.scatter_chart(data=df, x='ApplicantIncome', y='LoanAmount', color='Loan_Status')
-  
+
+#Data preparation
+with st.sidebar:
+  st.header('Input features')
+ 
+  Gender = st.selectbox('What is your gender?',('Female', 'Male'))
+  Married = st.selectbox('Are you married?', ('Yes', 'No'))
+  Dependents = st.selectbox('How many dependents do you have?', ('1', '2', '3+'))
+  Education = st.selectbox('What is your highest education level?',('Graduate', 'Not Graduate')) 
+  Self_Employed = st.selectbox('Are you self employed?', ('Yes', 'No'))
+  ApplicantIncome = st.slider('How much is your income?', 150, 81000, 5403)
+  CoapplicantIncome = st.slider('How much does your coapplicant earn?', 0, 41667, 1621)
+  LoanAmount = st.slider('How much are you applying?', 9, 700, 146)
+  Loan_Amount_Term = st.slider('How long do you want to repay the loan(in months)?', 12, 480, 342)
+  Credit_History = st.selectbox('Do you have some credit history?', 1, 0)
+
+  # Create a dataframe for the input features
+  data = {'Gender': Gender,
+          'Married': Married,
+          'Dependents': Dependents, 
+          'Education': Education, 
+          'Self_Employed': Self_Employed,
+          'ApplicantIncome': ApplicantIncome,
+          'CoapplicantIncome': CoapplicantIncome,
+          'LoanAmount': LoanAmount, 
+          'Loan_Amount_Term': Loan_Amount_Term, 
+          'Credit_History': Credit_History}
+  input_df = pd.DataFrame(data, index=[0])
+  input_loans = pd.concat([input_df, X], axis = 0)
+
+input_df
 
 
 
-st.cache_resource.clear()
+
   
   
