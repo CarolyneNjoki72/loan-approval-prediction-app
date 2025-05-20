18e5713a40576d6313b62c7877ce15a9e0ec3216
CarolyneNjoki72 <161461568+CarolyneNjoki72@users.noreply.github.com>
2025-05-20 16:37:28 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -63,11 +63,7 @@ with st.expander('Input features'):
   input_df
   st.write('**Combined loans data**')
   input_loans
-  st.write('**Encoded input loans**')
-  input_rows
 
 
-# Encode
-encode = ['Gender', 'Married', 'Dependents', 'Education', 'Self_Employed', 'Property_Area']
-df_loans = pd.get_dummies(input_loans, prefix=encode)
-input_rows = df_loans[:1]
+
+
